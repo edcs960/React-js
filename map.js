27fe2f5942b5803c5b756b704ed99f6659ac8a52
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-02 20:20:19 +0900

    카카오맵 api
    카카오맵 api를 이용하여 학교로 오는 버스들 노선 및 위치 확인하는 웹사이트

---
+++ map.js
@@ -0,0 +1,38 @@
+/*global kakao*/
+import React,{Component} from "react";
+import "./App.css";
+
+class BusMap extends Component {
+    componentDidMount(){
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = "dapi.kakao.com/v2/maps/sdk.js?appkey=d4ebffa4ca3e278c9226de88fff64612&autoload=false";
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            kakao.maps.load(()=>{
+                let container = document.getElementById("Busmap");
+                let options = {
+                    center: new kakao.maps.LatLng(37.506502, 127.053617),
+                    level: 3
+                };
+
+                let map = new window.kakao.maps.Map(container,options);
+            });
+        };
+
+        
+    }
+
+    
+
+    render() {
+        return (
+            <>
+                <div id="Busmap" Classname="Busmap"></div>
+            </>
+        );
+    }
+}
+
+export default BusMap;
