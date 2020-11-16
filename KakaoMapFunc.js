d8c29e1e926dd90789009e1872a2d588fc3fdd1d
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-16 18:49:53 +0900

    카카오맵 서버쪽 처리

--- KakaoMapFunc.js
+++ KakaoMapFunc.js
@@ -6,15 +6,13 @@ const cors = require("cors");
 require('dotenv').config();
 app.use(cors());
 
+var BusID;
 
-router.post("/", function (req, res) {
-  // 부산 버스 정보 시스템 api
+async function BusXml(BusLine) {
   var xhr = new XMLHttpRequest();
-  var url = 'http://61.43.246.153/openapi-data/service/busanBIMS2/busInfo'; /*URL*/
-  var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+process.env.BUS_API; /*Service Key*/
-  queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('-'); /**/
-  queryParams += '&' + encodeURIComponent('lineid') + '=' + encodeURIComponent('5200179000'); /**/
-  queryParams += '&' + encodeURIComponent('lineno') + '=' + encodeURIComponent('179'); /**/
+  var url = 'http://61.43.246.153/openapi-data/service/busanBIMS2/busInfoRoute'; /*URL*/
+  var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+ process.env.BUS_API; /*Service Key*/
+  queryParams += '&' + encodeURIComponent('lineid') + '=' + encodeURIComponent(BusLine); /**/
   xhr.open('GET', url + queryParams);
   xhr.onreadystatechange = function () {
       if (this.readyState == 4) {
@@ -22,9 +20,19 @@ router.post("/", function (req, res) {
       }
   };
 
+  xhr.send('');
+}
+
+router.post("/", function (req, res) {
+  // 부산 버스 정보 시스템 api
+  var WhatButton;
   var BusRefresh = req.body.Refresh;
+  var GeumjeongButton = req.body.Geumjeong;
+  var Bus301Button = req.body.Bus301;
+
   console.log(BusRefresh);
   res.end();
 });
 
 module.exports = router;
+
